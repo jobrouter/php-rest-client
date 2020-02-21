dbeb7d3a7fa76a55e33601b4cef4c57161d13a4e
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-02-21 11:04:18 +0100

    [TASK] Mark Version class as immutable

--- src/Information/Version.php
+++ src/Information/Version.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace Brotkrueml\JobRouterClient\Information;
 
+/**
+ * Value object that holds the current version of the library
+ * @psalm-immutable
+ */
 final class Version
 {
     private const VERSION = '0.9.0-dev';
