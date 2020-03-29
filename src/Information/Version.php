0c5034a902f80bf711bf9dfebcf12e5bf23f7071
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-03-29 15:48:28 +0200

    [RELEASE] Prepare version 0.9.1

--- src/Information/Version.php
+++ src/Information/Version.php
@@ -20,7 +20,7 @@ namespace Brotkrueml\JobRouterClient\Information;
  */
 final class Version
 {
-    private const VERSION = '0.9.1-dev';
+    private const VERSION = '0.9.1';
 
     public function getVersion(): string
     {
