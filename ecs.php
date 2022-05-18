dc4ae22216043d8cce131bd10ad28cee0f00183b
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-05-18 18:07:26 +0200

    [TASK] Update brotkrueml/coding-standards to version 2.0.0

--- ecs.php
+++ ecs.php
@@ -2,10 +2,9 @@
 
 declare (strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (ECSConfig $config): void {
     $header = <<<HEADER
 This file is part of the JobRouter Client.
 https://github.com/brotkrueml/jobrouter-client
@@ -16,28 +15,15 @@ For the full copyright and license information, please view the
 LICENSE.txt file that was distributed with this source code.
 HEADER;
 
-    $containerConfigurator->import(__DIR__ . '/vendor/brotkrueml/coding-standards/config/common.php');
+    $config->import(__DIR__ . '/vendor/brotkrueml/coding-standards/config/common.php');
 
-    $services = $containerConfigurator->services();
-    $services->set(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class)
-        ->call('configure', [[
-            'comment_type' => 'comment',
-            'header' => $header,
-            'separate' => 'both',
-        ]]);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(
-        Option::PATHS,
-        [
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-        ]
-    );
-
-    $parameters->set(Option::SKIP, [
-        \PHP_CodeSniffer\Standards\Generic\Sniffs\CodeAnalysis\AssignmentInConditionSniff::class . '.FoundInWhileCondition' => [
-            __DIR__ . '/tests/Unit/Mapper/RouteContentTypeMapperTest.php',
-        ]
+    $config->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+    $config->ruleWithConfiguration(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class, [
+        'comment_type' => 'comment',
+        'header' => $header,
+        'separate' => 'both',
     ]);
 };
