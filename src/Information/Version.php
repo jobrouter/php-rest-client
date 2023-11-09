a5dcbca1a07fef9f5fdc476b308b2056a09be6d0
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-11-09 19:19:36 +0100

    version: set to 2.1.0-dev

--- src/Information/Version.php
+++ src/Information/Version.php
@@ -19,7 +19,7 @@ namespace Brotkrueml\JobRouterClient\Information;
  */
 final class Version
 {
-    private const VERSION = '2.0.0';
+    private const VERSION = '2.1.0-dev';
 
     public function getVersion(): string
     {
